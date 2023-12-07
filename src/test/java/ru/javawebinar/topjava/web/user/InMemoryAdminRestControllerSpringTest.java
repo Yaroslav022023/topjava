7a44a94166ecdea1ffb4299a7378cd8dde7b4778
Yaroslav <set.oplata@gmail.com>
2023-12-07 19:37:22 +0100

    Redesigned xml configurations for Spring

--- src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
@@ -15,8 +15,7 @@ import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 @ContextConfiguration({
         "classpath:spring/spring-app.xml",
-        "classpath:spring/spring-db.xml",
-        "classpath:spring/spring-app-inmemory-test.xml"
+        "classpath:spring/spring-inmemory-test.xml"
 })
 @RunWith(SpringRunner.class)
 public class InMemoryAdminRestControllerSpringTest {
--- src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerTest.java
@@ -23,7 +23,7 @@ public class InMemoryAdminRestControllerTest {
     @BeforeClass
     public static void beforeClass() {
         appCtx = new ClassPathXmlApplicationContext("classpath:spring/spring-app.xml",
-                "classpath:spring/spring-db.xml", "classpath:spring/spring-app-inmemory-test.xml");
+                "classpath:spring/spring-inmemory-test.xml");
         log.info("\n{}\n", Arrays.toString(appCtx.getBeanDefinitionNames()));
         controller = appCtx.getBean(AdminRestController.class);
         repository = appCtx.getBean(InMemoryUserRepository.class);
